579dd2f37ccc7e86a8e98b52d4e0d518f86b9df6
Isal <58117968+rfzsal@users.noreply.github.com>
2022-07-29 10:56:08 +0700

    Update web name

--- blog.config.js
+++ blog.config.js
@@ -3,7 +3,7 @@ const BLOG = {
   author: 'Faizal',
   email: 'faizal.fazri@raharja.info',
   link: '',
-  description: 'BigBoyz Blog',
+  description: '',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   appearance: 'auto', // ['light', 'dark', 'auto'],
   font: 'sans-serif', // ['sans-serif', 'serif']
